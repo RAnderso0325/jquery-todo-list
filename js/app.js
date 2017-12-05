8b559187e6bcf96690e252b0616cc8610eb7093e
RAnderso0325 <reillyjanderson@gmail.com>
2017-12-04 18:22:42 -0800

    added completed list and logic for remove item button

--- js/app.js
+++ js/app.js
@@ -1,11 +1,3 @@
-// $('ul.myclass li span').click( function() {
-//     var $cb = $(this).parent().find(":checkbox");
-//     if (!$cb.prop("checked")) {
-//         $cb.prop("checked", true);
-//     } else {
-//         $cb.prop("checked", false);
-//     }
-// });
 $(document).ready(function(){
 	 $('.form1').submit(function(e){
       e.preventDefault();
@@ -20,16 +12,7 @@ $('#create-item').click(function(){
 	$('#GET-NEW-ITEM').val("");
 });
 
-
-
-// <textarea title="Write a comment..." itemID="@item.ID" 
-// class="comment" placeholder="Write a comment..." 
-// autocomplete="off" role="textbox" id="comment"></textarea>
-// e.preventDefault()
-
-//make add item on preshing return
-//text box for user to enter a list item
-//reset the text box AFTER each list item is added
-//make that new item check off-able
-//EITHER cross off completed ('checked') items
-//OR move to new list
+$('.list').on('click', 'li', function(e){
+	$(this).css("text-decoration", "line-through");
+	$('.comp-list').append(this).append($('<button>').text("Remove"));
+});
